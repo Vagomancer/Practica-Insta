d3205296e827011245f6f49296e99f82b2b8c75a
Crisale7 <cristhian.yanez@ucb.edu.bo>
2024-02-21 01:57:06 -0400

    Implementado de notificacion push y simulacion funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { FcmService } from './services/fcm.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private fcm: FcmService
+  ) {
+    this.platform.ready().then(() => {
+      this.fcm.initPush();
+    }).catch(e => {
+      console.log('error fcm: ', e);
+    });
+  }
 }
